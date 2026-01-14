e8d6ff6df5f80977d7030bc257d8e4148ff03e85
sarthak0401 <sarthakbokade413@gmail.com>
2026-01-14 12:06:29 +0530

    Learnt es6 fundamentals including async-await-fetch, map, filter, some, indexOf, findIndex, etc.Moving to learn react fundamentals

--- Es6 fundamentals/script.js
+++ Es6 fundamentals/script.js
@@ -10,24 +10,14 @@ console.log(b && a); // This will only check for the first expression only, if i
 console.log(a || console.log("Meow"));
 console.log("Meow" || a);
 
-
-
 const user_name = "Sarthak";
-
-
 
 // Template literals
 console.log(`Hi there, ${user_name}`);
 
-
-
-
 // Ternary operator
 const x = true;
 x ? console.log("Condition true") : console.log("Condition false");
-
-
-
 
 // De-structuring arrays and objects (Using short-hand's)
 
@@ -88,9 +78,6 @@ const [num1, num2, num3, num4, num5] = arr;
 console.log(num1, num2, num3, num4, num5);
 // See the fifth one will be showing undefined
 
-
-
-
 // Default parameters, spread and rest operators
 
 // Default parameters
@@ -102,8 +89,6 @@ console.log(multOfTwoNumbers()); // If we dont pass any arguments it will consid
 console.log(multOfTwoNumbers(1, 2)); // THis will override the default parameters
 
 console.log(multOfTwoNumbers(15)); // This will be considered as the first parameter and second parameter's value will be taken from default one
-
-
 
 // Spread operator
 const arr2 = [1, 2, 3, 4, 5];
@@ -119,8 +104,6 @@ console.log([...arr2, ...arr3]); // we can merge two arrays into other array as 
 console.log([10010, ...arr2, 55, ...arr3, 9923929]); // This creates a whole array including this elements
 
 //  ... -> this is the spread operator
-
-
 
 // Rest operator
 function getVal(a, b, c) {
@@ -142,15 +125,18 @@ console.log(getVal(1, 2, 4, 5, 6, 7, 8, 8, 9, 90, 78, 9768, 65)); // This will t
 
 // NOTE : The rest operator (parameter) ALWAYS have to be at the end of the parameter list. THis is Not acceptable : function getVal(...c, a,b){}
 
-
-
-
 // map, filter, find, some, every, includes, indexOf, findIndex
 
-const person = [  // this is the array of objects
+const personArray = [
+  // this is the array of objects
   {
     name: "Person 1",
     age: 22,
+    country: "India",
+  },
+  {
+    name: "Person x",
+    age: 26,
     country: "India",
   },
   {
@@ -177,12 +163,109 @@ Index (the numerical position)
 Array (the original array being mapped)
 */
 
-
 // singlePerson is the current element
-let getAllNames = person.map((singlePerson, index) => {
-    console.log(singlePerson, index);
-    // return singlePerson.name;
-    return `Name : ${singlePerson.name}, Age : ${singlePerson.age}`
+let getAllNames = personArray.map((singlePerson, index) => {
+  console.log(singlePerson, index);
+  // return singlePerson.name;
+  return `Name : ${singlePerson.name}, Age : ${singlePerson.age}`;
 });
 
 console.log(getAllNames);
+
+// find
+
+/*find also returns, value, index and object */
+let getPersonFromIndia = personArray.find((singlePerson, index) => {
+  return singlePerson.country === "India";
+});
+
+console.log(getPersonFromIndia);
+// NOTE : If we have multiple objects with SAME country name, find will ONLY return the first matching object, NOT ALL(IMP)
+
+// filter
+
+// filter will return all the matching objects in an Array of condition satisfying objects
+
+let getAllPersonsFromIndia = personArray.filter((person, index) => {
+  return person.country === "India"; // This is the condition checking step
+});
+
+console.log(getAllPersonsFromIndia);
+// This will give all the matching objects in an Array
+
+// some
+
+// some   -> returns boolean value, if atleast one object satisfies the condition it returns true, else if no objects satisfies then it returns false
+
+let checkSomeArrayMethodExample = personArray.some((person, index) => {
+  return person.age > 27;
+});
+
+console.log(checkSomeArrayMethodExample);
+
+// every
+
+// every -> returns boolean value, but it checks the condition on every objects, and if all satisfies then it returns true, else false
+
+let checkEveryArrayMethodExample = personArray.every((singlePerson) => {
+  return singlePerson.age > 20;
+});
+
+console.log(checkEveryArrayMethodExample);
+// If the condition is true for all the objects of the array then every will return true, else false
+
+// includes
+
+// includes   -> this is basically like contains
+const fruits = ["Apple", "Banana", "Orange"];
+console.log(fruits.includes("Apple"));
+
+// If the element is present in the array it will return true, else false
+
+// indexOf
+
+// It returns the index of the element if present in the array, else returns -1;
+
+console.log(fruits.indexOf("Apple")); // returns 0
+console.log(fruits.indexOf("Mango")); // returns -1
+
+// findIndex
+
+let getIndexOfPersonWhoIsFromSweden = personArray.findIndex((person, index) => {
+  return person.country === "Sweden";
+});
+
+console.log(getIndexOfPersonWhoIsFromSweden);
+// returns index if the element is present, else return -1
+
+// async, await and fetch
+// async is written before the function to say that there will be some waiting in this function
+
+// await keyword is used before fetch method to indicate to wait till the data is fetched into the method
+
+const getListOfProductsElement = document.querySelector(
+  ".list-of-products-from-api"
+);
+
+function renderProduct(getProduct) {
+  getListOfProductsElement.innerHTML = getProduct.map(singleProductItem => `<p>${singleProductItem.title}</p>`).join(" ");
+}
+
+async function fetchListOfProducts() {
+  try {
+    const apiResponse = await fetch(`https://dummyjson.com/products`, {
+      method: "GET",
+    });
+
+    const result = await apiResponse.json();
+    console.log(result);
+
+    if (result?.products?.length > 0) renderProduct(result?.products);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+fetchListOfProducts();
+
+// This displays the list of product from the api -> interesting one!
