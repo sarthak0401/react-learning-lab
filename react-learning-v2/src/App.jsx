dfb40e20d6dd5ce17a9c60f557a61e114340ec78
sarthak0401 <sarthakbokade413@gmail.com>
2026-02-23 23:15:27 +0530

    Learnt about what is jsx and how to use js inside jsx file.

--- react-learning-v2/src/App.jsx
+++ react-learning-v2/src/App.jsx
@@ -4,6 +4,8 @@ import { Button } from "./components/Button";
 
 function App() {
   const [count, setCount] = useState(0);
+  const meow = <h1> hello </h1>;
+
   return (
     <>
       <div>
@@ -11,7 +13,12 @@ function App() {
         <p>Started my react journey</p>
       </div>
       <Button setCount={setCount} />
+      {/* We use {} curly braces to use js expression inside it */}
       <p>{count}</p>
+      <p>{meow}</p>
+      <p style={{ color: "red", textDecoration: "underline" }}>
+        This is the styled text!
+      </p>
     </>
   );
 }
